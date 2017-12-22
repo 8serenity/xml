de985bcffc87dab66304b0d43a87fff29ff4b0be
Niyaz <mukhamedya.n@gmail.com>
2017-12-22 11:11:02 +0600

    xml practice

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+/*1.	С помощью класса XDocument прочитать XML файл
+ * и вывести название всех его тегов с сохранением иерархии. В качестве отступов для иерархии использовать 2 пробела.*/
+
+
+namespace XMLPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Users.xml");
+            XDocument documentNew = new XDocument();
+
+            XmlElement xRoot = xDoc.DocumentElement;
+            documentNew.Tags.Add(xRoot.Name);
+            documentNew.Tags.Add("  ");
+            foreach (XmlNode xnode in xRoot)
+            {
+                documentNew.Tags.Add(xnode.Name);
+                documentNew.Tags.Add(" ");
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    documentNew.Tags.Add(childnode.Name);
+                    documentNew.Tags.Add(" ");
+                }
+                documentNew.Tags.Add("  ");
+            }
+
+
+            for (int i = 0; i < documentNew.Tags.Count; i++)
+            {
+                Console.Write(documentNew.Tags[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ XDocument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLPractise
+{
+    class XDocument
+    {
+        public List<string> Tags { get; set; }
+        
+
+        public XDocument()
+        {
+            Tags = new List<string>();
+        }
+
+
+    }
+}
